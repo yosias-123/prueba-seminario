caae1b0427a243484a5ef761b412f3ba3bd9ca5e
Jose Yosias Samallani Sisniegas <yosiassamallani@gmail.com>
2026-04-21 21:03:28 -0500

    fex: Actualizacion del archivo db.js

--- crud-vendedores/config/db.js
+++ crud-vendedores/config/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2/promise");
 
+// Actualizacion del root
 const pool = mysql.createPool({
-  host: "yamabiko.proxy.rlwy.net",
+  host: "ballast.proxy.rlwy.net",
   user: "root",
-  password: "nKgSPhSJYWsqwwxZtZSBnYCoUtqlSgox",
+  password: "qlhjQnyksqNlMrWFnvvsjCztQAZqFavv",
   database: "railway",
-  port: 47307,
+  port: 47127,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
